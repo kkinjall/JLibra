d6b71f84155cedd7a761683968aeb1d69bf5ac0d
Kinjal Kamboj <kinjalkam16@gmail.com>
2025-10-05 13:01:02 -0400

    Initial Setup

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,7 @@
+package org.example;
+
+/*Main file that initiates and runs the library system*/
+public class Main {
+    public static void main(String[] args) {
+    }
+}
---
+++ src/test/java/org/example/MainTest.java
@@ -0,0 +1,6 @@
+package org.example;
+
+/*Test file for all Responsibilities and corresponding unit tests*/
+public class MainTest {
+
+}
