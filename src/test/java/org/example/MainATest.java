c3d92424498902be6bcccef2d198c333527a53ca
Kinjal Kamboj <kinjalkam16@gmail.com>
2025-10-11 22:52:03 -0400

    A-TEST-02 Initialize library system and handle authentication with error handling

--- src/test/java/org/example/MainATest.java
+++ src/test/java/org/example/MainATest.java
@@ -72,4 +72,43 @@ public class MainATest {
         library.displayBookCollection(scanner, output);
         assertTrue(outputString.toString().contains("15. Title: The Great Gatsby, Author: F. Scott Fitzgerald, Status: AVAILABLE"));
     }
+
+    @Test
+    @DisplayName("A-TEST-02: Initialization and Authentication with Error Handling")
+    void A_TEST_02() {
+        String input =
+                //User1 has valid login
+                "spongebob\nilovegary!\n" +
+                        //User1 logout
+                        "y\n" +
+                        //User2 login with invalid credentials
+                        "squidwardTent\n0clarinet_fan0\n";
+
+        Scanner scanner = new Scanner(input);
+        StringWriter outputString = new StringWriter();
+        PrintWriter output = new PrintWriter(outputString);
+
+        Library library = new Library();
+        library.initializeLibrary();
+
+        //Check initialization
+        assertEquals(20, library.getNumBooks());
+        assertEquals(3, library.getNumBorrowers());
+
+        //Valid login
+        library.authenticateUser(scanner, output);
+        assertTrue(outputString.toString().contains("Authentication successful!"));
+        library.displayMenu(output);
+        assertTrue(outputString.toString().contains("--- Library Menu ---\n" +
+                "1. Borrow a book\n" +
+                        "2. Return a book\n" +
+                        "3. Logout\n" +
+                        "Enter choice (1-3): "));
+        library.logout(scanner, output);
+        assertTrue(outputString.toString().contains("You have successfully logged out."));
+
+        //Invalid login
+        library.authenticateUser(scanner, output);
+        assertTrue(outputString.toString().contains("Authentication unsuccessful"));
+    }
 }
