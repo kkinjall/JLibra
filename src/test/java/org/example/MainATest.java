d08b2e8a1242073000f9abe95f9dd113b5b9bf4e
Kinjal Kamboj <kinjalkam16@gmail.com>
2025-10-11 22:51:03 -0400

    A-TEST-01 Acceptance test for allowing multiple users to login, view availability of books, borrow and return

---
+++ src/test/java/org/example/MainATest.java
@@ -0,0 +1,75 @@
+package org.example;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.Scanner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MainATest {
+    @Test
+    @DisplayName("A-TEST-01: Multi-User Borrow and Return with Availability Validated")
+    void A_TEST_01() {
+        String input =
+                //User1 logs in
+                "spongebob\nilovegary!\n" +
+                        //menu, borrow
+                        "1\n" +
+                        //choose book 15 ("The Great Gatsby")
+                        "15\ny\n\n" +
+                        //User1 logout
+                        "y\n" +
+
+                        //User2 logs in
+                        "sandy_cheeks\ntexasgurl004\n" +
+                        //menu, borrow
+                        "1\n" +
+                        //choose book 15 again (checked out)
+                        "15\n" +
+                        //confirm attempt, decline to place hold
+                        "y\nn\n" +
+                        //User2 logout
+                        "y\n";
+
+        Scanner scanner = new Scanner(input);
+        StringWriter outputString = new StringWriter();
+        PrintWriter output = new PrintWriter(outputString);
+
+        Library library = new Library();
+        library.initializeLibrary();
+
+        //User1 login, borrow book, and logout
+        library.authenticateUser(scanner, output);
+        library.displayMenu(output);
+        library.displayBookCollection(scanner, output);
+        library.selectBookToBorrow(scanner, output);
+        assertTrue(outputString.toString().contains("You have successfully borrowed The Great Gatsby"));
+        library.logout(scanner, output);
+
+        //User2 login and see it is Checked Out
+        library.authenticateUser(scanner, output);
+        library.displayMenu(output);
+        library.displayBookCollection(scanner, output);
+        assertTrue(outputString.toString().contains("15. Title: The Great Gatsby, Author: F. Scott Fitzgerald, Status: CHECKED_OUT, Due:"));
+        library.selectBookToBorrow(scanner, output);
+        library.logout(scanner, output);
+
+        //User1 logs in, returns borrowed book
+        input = "spongebob\nilovegary!\n1\ny\n\ny\n";
+        scanner = new Scanner(input);
+        library.authenticateUser(scanner, output);
+        library.returnBook(scanner, output);
+        assertTrue(outputString.toString().contains("You have successfully returned The Great Gatsby"));
+        library.logout(scanner, output);
+
+        //User2 logs in, sees book is available
+        input = "sandy_cheeks\ntexasgurl004\n1";
+        scanner = new Scanner(input);
+        library.authenticateUser(scanner, output);
+        library.displayBookCollection(scanner, output);
+        assertTrue(outputString.toString().contains("15. Title: The Great Gatsby, Author: F. Scott Fitzgerald, Status: AVAILABLE"));
+    }
+}
